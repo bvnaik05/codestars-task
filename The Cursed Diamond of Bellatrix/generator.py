3d981a71464e4d37e87d2a33b427a4ed155d2c76
Harssh <>
2024-11-03 17:03:11 +0530

    changes made as said

--- The Cursed Diamond of Bellatrix/generator.py
+++ The Cursed Diamond of Bellatrix/generator.py
@@ -14,21 +14,17 @@ with open(file_path, 'r') as file:
     spells_content = file.read()
 
 spells_list = list(map(str.strip, spells_content.split("\n")))
-total_tc = 10
+total_tc = 15
 tc_count = 0
 base_file_name = f"./Test Cases"
-
+shuffle_spells(spells=spells_list)
 for i in range(total_tc):
     tc_count = i+1
     input_file_name = f"{base_file_name}/input{tc_count}.txt"
     output_file_name = f"{base_file_name}/output{tc_count}.txt"
     input_file = open(input_file_name,"w")
-    t = random.randint(1,10)
-    input_file.write(f"{t}\n")
-    shuffle_spells(spells=spells_list)
-    for i in range(t):
-        input_file.write(f"{len(spells_list[i])}\n")
-        input_file.write(f"{spells_list[i]}\n")
+    input_file.write(f"{len(spells_list[i])}\n")
+    input_file.write(f"{spells_list[i]}\n")
     with open(input_file_name, 'r') as input_file, open(output_file_name, 'w') as output_file:
         subprocess.run(["soln.exe"], stdin=input_file, stdout=output_file, check=True)
 
